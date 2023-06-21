c0421b0044195b27b63ae86a62cca39836369ee9
Nahuel Santillan <nahueltecnicomdp@gmail.com>
2023-06-21 02:36:14 -0300

    layoutttt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import './globals.css'
 import TabsMenu from '@/components/TabsMenu'
 
 export const metadata = {
-  title: 'Spirit Studio',
-  description: '',
+  title: 'Spirit Studio | Clothes',
+  description: 'Bienvenido a Spirit Studio 79, donde el estilo se encuentra con la autoafirmación. Descubre nuestras colecciones únicas y exprésate a lo grande. Únete a nuestra comunidad y libera tu espíritu con cada prenda.',
 }
 
 export default function RootLayout({
--- src/app/pagar/layout.tsx
+++ src/app/pagar/layout.tsx
@@ -2,7 +2,7 @@ import TabsMenu from "@/components/TabsMenu"
 
 export const metadata = {
   title: 'Pagar | Spirit Studio',
-  description: '',
+  description: 'Bienvenido a Spirit Studio 79, donde el estilo se encuentra con la autoafirmación. Descubre nuestras colecciones únicas y exprésate a lo grande. Únete a nuestra comunidad y libera tu espíritu con cada prenda.',
 }
 
 export default function Layout({
